21e76bae1676731f81d5cba65a00f28feef61894
Evhen Kassa <kassa1992@ukr.net>
2023-09-15 13:32:15 +0300

    Deny access example

--- routes.js
+++ routes.js
@@ -5,3 +5,13 @@ import { Router, edgioRoutes, HTTP_HEADERS } from '@edgio/core'
 export default new Router()
   // automatically adds all routes from the Node.js connector
   .use(edgioRoutes)
+
+  // will deny access for all pages with denyAccess=true in url
+  .if(
+    {
+      edgeControlCriteria: {
+        "===": [{ "request.origin_query": "denyAccess" }, "true"],
+      },
+    },
+    { access: { deny_access: true } } //
+  );
